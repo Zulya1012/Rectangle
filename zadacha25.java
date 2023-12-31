4a7fa5051075a01510e8597536c582f7738c0e80
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-31 14:30:56 +0400

    Add files via upload

---
+++ zadacha25.java
@@ -0,0 +1,10 @@
+public class zadacha25 { 
+public static void main(String[] strings) {
+double a = 5.6;
+double b = 8.5;
+double perimeter = 2 * (a + b);
+double area = a * b;			
+System.out.println("Perimeter:" + +2 * + (a + b));
+System.out.println("Area:" + +a *+ +b);
+    }
+}
